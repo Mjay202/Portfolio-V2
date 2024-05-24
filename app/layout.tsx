2ebaf52930576f0cb00e6edf16a9e31f46bc25ad
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-24 01:24:50 +0100

    working on about me

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SlSocialGithub, SlSocialInstagram, SlSocialLinkedin, SlSocialTwitter } from "react-icons/sl";
+import { SiHandshakeProtocol } from "react-icons/si";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,11 +23,119 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Signika:wght@300..700&display=swap" rel="stylesheet"></link>
       </head> */}
-      <body className={` ${inter.className}`}>
+      <body className={`relative ${inter.className}`}>
+        <nav className="fixed top-0 left-0 right-0 bg-black">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 px-10">
+            {/* <Image
+            className="hover:animate-bounce"
+            src="/logo.png"
+            alt="Next.js Logo"
+            width={80}
+            height={80}
+            priority
+          /> */}
+            <SiHandshakeProtocol className="text-emerald-400 text-4xl hover:animate-spin" />
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <ul className="font-medium flex items-center flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    <span className="text-sm font-mono mr-1 text-emerald-400">
+                      {" 01."}
+                    </span>
+                    <span className="text-md hover:text-emerald-400 font-mono text-slate-300">
+                      {"Home"}
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    <span className="text-sm font-mono mr-1 text-emerald-400">
+                      {" 02."}
+                    </span>
+                    <span className="text-md hover:text-emerald-400 font-mono text-slate-300">
+                      {"Experience"}
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    <span className="text-sm font-mono mr-1 text-emerald-400">
+                      {" 03."}
+                    </span>
+                    <span className="text-md hover:text-emerald-400 font-mono text-slate-300">
+                      {"Projects"}
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    <span className="text-sm font-mono mr-1 text-emerald-400">
+                      {" 04."}
+                    </span>
+                    <span className="text-md hover:text-emerald-400 font-mono text-slate-300">
+                      {"Contact"}
+                    </span>
+                  </a>
+                </li>
+                <li className="place-content-center">
+                  <button
+                    type="button"
+                    className="hover:border-r-4 font-mono  hover:border-b-4 py-2 px-5 me-2 mb-2 text-md font-medium  focus:outline-none bg-transparent rounded-lg border border-emerald-400 transition ease-out duration-300 hover:scale-105 text-emerald-400 focus:z-10 focus:ring-2 focus:ring-emerald-100"
+                  >
+                    Resume
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         {children}
 
         {/* Aside Socials Icons */}
-        <div className="fixed bottom-0 left-12 xs:hidden md:flex flex-col gap-0 items-center ">
+        <div className="fixed bottom-0 md:left-12 sm:left-8 hidden sm:flex flex-col gap-0 items-center ">
           {" "}
           <div className="flex flex-col gap-7 align-items-center  justify-center mt-4 sm:justify-center sm:mt-0">
             <a
@@ -62,7 +171,7 @@ export default function RootLayout({
         </div>
 
         {/* Aside Email Right */}
-        <div className="fixed bottom-0 right-12 flex flex-col gap-0 items-center">
+        <div className="fixed bottom-0 right-12 hidden sm:flex flex-col gap-0 items-center">
           <span className="text-slate-400 text-sm vert-text font-mono tracking-wider hover:text-emerald-400 duration-300 hover:scale-105 cursor-pointer transition ease-out ">
             mujeeb.o.hammed@gmail.com
           </span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,147 +1,139 @@
 "use client";
-import Image from "next/image";
-import { use, useEffect, useState } from "react";
+import { PiHandWavingLight } from "react-icons/pi";
+import { SiHandshakeProtocol } from "react-icons/si";
 export default function Home() {
 
-  const [scroll, setscroll] = useState(false);
   
-  useEffect(() => {
-    seeScroll();
-  }, [scroll])
-  
-  let scrollPosition = window.scrollY;
-  const seeScroll = () => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        setscroll(true);
-      }
-      if (window.scrollY == 0 || window.screenY < scrollPosition) {
-        setscroll(false);
-      }
-    });
-  }
   
   return (
     <main className="">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
 
-      <nav className={`z-10 ${!scroll ? "" : "hidden"}`}>
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 px-10">
-          <Image
-            className="hover:animate-bounce"
-            src="/logo.png"
-            alt="Next.js Logo"
-            width={80}
-            height={80}
-            priority
-          />
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex items-center flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  <span className="text-sm font-mono mr-1 text-emerald-400">
-                    {" 01."}
-                  </span>
-                  <span className="text-md font-mono text-slate-300">
-                    {"Home"}
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  <span className="text-sm font-mono mr-1 text-emerald-400">
-                    {" 02."}
-                  </span>
-                  <span className="text-md font-mono text-slate-300">
-                    {"Experience"}
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  <span className="text-sm font-mono mr-1 text-emerald-400">
-                    {" 03."}
-                  </span>
-                  <span className="text-md font-mono text-slate-300">
-                    {"Projects"}
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  <span className="text-sm font-mono mr-1 text-emerald-400">
-                    {" 04."}
-                  </span>
-                  <span className="text-md font-mono text-slate-300">
-                    {"Contact"}
-                  </span>
-                </a>
-              </li>
-              <li className="place-content-center">
-                <button
-                  type="button"
-                  className="transition ease-in-out bg-transparent border  focus:outline-none font-mono hover:shadow-lg focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-md px-5 py-2 me-2 mb-2 delay-150 border-emerald-400 text-emerald-400 hover:-translate-y-1 hover:scale-105  duration-300"
-                >
-                  Resume
-                </button>
-              </li>
-            </ul>
-          </div>
+      {/* Hero Section */}
+      <div className="px-10 md:px-36 sm:px-28 mt-24 font-inter">
+        <h4 className="sm:text-sm lg:text-md text-xs text-teal-300 font-mono">
+          {" "}
+          Hello{" "}
+          <span className="inline-flex">
+            <PiHandWavingLight className="ml-1 hover:animate-ping" />
+          </span>
+        </h4>
+        <h3 className="sm:text-lg text-md text-teal-300 sm:mt-4  mt-2 font-mono">
+          Welcome, my name is
+        </h3>
+        <div className="relative z-[-1] before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-emerald-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-emerald-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#01ffd0] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+          <h3 className="lg:text-7xl md:text-6xl sm:text-5xl text-slate-200 lg:mt-12 md:mt-7 mt-5 font-bold text-4xl">
+            Mujeeb Hammed.
+          </h3>
         </div>
-      </nav>
+        <h3 className="lg:text-7xl md:text-6xl sm:text-5xl text-slate-400 mt-2 sm:mt-5 font-bold text-4xl">
+          I build all the backend stuffs.
+        </h3>
+        <p className="text-slate-400 mt-9 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter font-relaxed">
+          Senior Software Developer with extensive experience in building
+          fullstack solutions. Specializing in backend development, I excel in
+          creating robust, scalable applications using
+          <span className="text-emerald-400"> PHP Laravel </span> and
+          <span className="text-emerald-400"> JavaScript Node.js</span>, my
+          preferred stacks.
+        </p>
+        <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter font-relaxed">
+          With a strong foundation in both frontend and backend technologies, I
+          have successfully led and contributed to numerous projects across
+          various industries. My expertise includes designing complex databases,
+          optimizing application performance, and implementing secure
+          authentication systems. Passionate about clean code and best
+          practices, I am dedicated to delivering high-quality software that
+          meets user needs and business goals.
+        </p>
 
-      <div className="px-36 mt-10 font-inter">
-        <h4 className="text-sm text-teal-300 font-mono"> Hello</h4>
-        <h3 className="text-lg text-teal-300 mt-4 font-mono">Welcome, my name is</h3>
-        <h3 className="text-7xl text-slate-300 mt-7 font-bold sm:text-2xl">Mujeeb Hammed.</h3>
-        <h3 className="text-7xl text-slate-500 mt-7 font-bold sm:text-2xl">Senior Backend Developer.</h3>
+        <button
+          type="button"
+          className="mt-10 hover:border-r-4 font-mono  hover:border-b-4 py-4 px-9 me-2 mb-2 text-md font-medium  focus:outline-none bg-transparent rounded-lg border border-emerald-400 transition ease-out duration-300 hover:scale-105 text-emerald-400 focus:z-10 focus:ring-2 focus:ring-emerald-100"
+        >
+          Hire me Now!
+        </button>
       </div>
 
-      {/* <div className="relative z-[-1] flex justify-between mt-20 px-20 place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-emerald-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-emerald-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#01ffd0] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      {/* <div className="relative z-[-1]  mt-20 px-20 place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-emerald-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-emerald-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#01ffd0] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <div>Name</div>
         <div>Another</div>
       </div> */}
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left"></div>
+
+      {/* ABOUT  */}
+      <div className="px-10 md:px-36 sm:px-28 mt-10 font-inter flex ">
+        <div>
+          <div className="flex items-center justify-items-center ">
+            <span className="mr-3 font-mono text-lg text-emerald-400">01.</span>
+            <h2 className="mr-4 text-2xl text-slate-300 font-bold">About Me</h2>
+            <span>
+              <hr className="self-center h-0.1 w-64 bg-emerald-400 mb-12 inline-flex" />
+            </span>
+          </div>
+          <div>
+            <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter lg:text-relaxed font-relaxed tracking-wide">
+              Hello, my name is Mujeeb Hammed. I am a
+              <span className="text-emerald-400">
+                {" seasoned Senior Software Developer "}
+              </span>
+              with a passion for building innovative and efficient backend
+              solutions. My journey in software development
+              <span className="text-emerald-400">
+                {" started back in 2015 "}
+              </span>
+              when I was creating website pages for small businesses of friends
+              and family. This early experience sparked my interest and led me
+              to pursue further education in Software Engineering at
+              <span className="text-emerald-400">{" SQI College of ICT"}</span>,
+              a prestigous institute of learning SWE in Nigeria.
+            </p>
+            <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter lg:text-relaxed font-relaxed tracking-wide">
+              During my studies, I picked up various stacks like
+              <span className="text-emerald-400"></span> PHP Laravel and
+              JavaScript Node.js, and also became proficient with frontend
+              frameworks such as <span className="text-emerald-400"></span>
+              React, Vue.js, Blade, and Next.js. I have had the opportunity to
+              <span className="text-emerald-400">
+                {
+                  " lead and collaborate  on numerous side projects with like-minded professional teams"
+                }
+              </span>
+              , further enhancing my skills and knowledge.
+            </p>
+            <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter lg:text-relaxed font-relaxed tracking-wide">
+              Fast forward to today, I am currently working with an
+              <span className="text-emerald-400">{""}</span>educational
+              institution where I have created and actively maintaining a
+              Library Management System
+              <span className="text-emerald-400">{""}</span>. This role has
+              allowed me to apply my expertise in backend development and
+              continue my commitment to delivering high-quality software
+              solutions.
+            </p>
+            <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter lg:text-relaxed font-relaxed tracking-wide">
+              Throughout my career, I have designed and managed complex
+              databases, optimized application performance, and implemented
+              secure authentication systems. I am dedicated to writing clean,
+              maintainable code and adhering to best practices to ensure the
+              highest quality in every project I undertake.
+            </p>
+            <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter lg:text-relaxed font-relaxed tracking-wide">
+              I am passionate about technology and continuously seek to stay
+              updated with the latest trends and advancements in the field.
+              Whether it's collaborating with a team or working independently, I
+              strive to deliver solutions that not only meet but exceed client
+              expectations.
+            </p>
+            <p className="text-slate-400 mt-9 mb-3 lg:mt-12 pr-16 lg:pr-60 text-md lg:text-lg font-inter lg:text-relaxed font-relaxed tracking-wide">
+              Outside of coding, I enjoy exploring new technologies,
+              contributing to open-source projects, and sharing knowledge with
+              the developer community.
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </main>
   );
 }
